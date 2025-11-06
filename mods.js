fb1c8eb55d24e093a8bf60627f8e46dfadcbaea1
SLIDEboo <edgasmo@hotmail.com>
2025-11-06 19:23:18 +0100

    Create mods.js

---
+++ mods.js
@@ -0,0 +1,7 @@
+[
+  {
+    "id": "1OousP2Cls9R30zS3CqiYbqYW3zAlpjrZ",
+    "nombre": "aquamirae-6.API15.jar",
+    "url": "https://drive.google.com/uc?export=download&id=1OousP2Cls9R30zS3CqiYbqYW3zAlpjrZ"
+  }
+]
