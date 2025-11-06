cc0c296a13fe0ab3eb1c383e10aa4f44d767e61d
SLIDEboo <edgasmo@hotmail.com>
2025-11-06 19:47:21 +0100

    Update mods.js

--- mods.js
+++ mods.js
@@ -4,9 +4,4 @@
     "nombre": "aquamirae-6.API15.jar",
     "url": "https://drive.google.com/uc?export=download&id=1OousP2Cls9R30zS3CqiYbqYW3zAlpjrZ"
   }
-    {
-    "id": "1OousP2sdasdCls9R30zS3CqiYbqYW3zAlpjrZ",
-    "nombre": "temples.jar",
-    "url": "https://drive.google.com/uc?export=download&id=1cYQzWnk1vY3QQvSBlUx3-uGembowq2Nb"
-  }
-  ]
+]
