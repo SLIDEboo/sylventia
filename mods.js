ac6b24f31d50afb28509f04133425a8127aa96de
SLIDEboo <edgasmo@hotmail.com>
2025-11-07 14:21:28 +0100

    Update mods.js

--- mods.js
+++ mods.js
@@ -1,29 +1,14 @@
 {
-  "minecraft_version": "1.20.1",
-  "loader": "fabric",
-  "loader_version": "0.15.11",
-  "mods": [
+ [
     {
-      "name": "Fabric API",
-      "filename": "fabric-api.jar",
-      "url": "https://drive.google.com/uc?export=download&id=1OousP2Cls9R30zS3CqiYbqYW3zAlpjrZ&confirm=t",
-      "hash": sha1:abc123...,
-      "required": true
-    },
+   "url": "https://drive.google.com/uc?export=download&id=1OousP2Cls9R30zS3CqiYbqYW3zAlpjrZ&confirm=t",
+ },
     {
-      "name": "Sodium",
-      "filename": "sodium.jar",
-      "url": "https://drive.google.com/uc?export=download&id=1cYQzWnk1vY3QQvSBlUx3-uGembowq2Nb&confirm=t",
-      "hash": sha1:a221bc123...,
-      "required": true
-    },
-        {
-      "name": "Sodiussm",
-      "filename": "sodiussm.jar",
-      "url": "https://drive.google.com/uc?export=download&id=12Z0deuYfx5Z1Kpnx7ShcVlfqbZ-q87gU&confirm=t",
-      "hash": sha1:a11221bc123...,
-      "required": true
-    }
+ "url": "https://drive.google.com/uc?export=download&id=1cYQzWnk1vY3QQvSBlUx3-uGembowq2Nb&confirm=t",
+ },
+   {
+   "url": "https://drive.google.com/uc?export=download&id=12Z0deuYfx5Z1Kpnx7ShcVlfqbZ-q87gU&confirm=t",
+   }
     
   ]
 }
