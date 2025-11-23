6ae39c98ac3cb3db6623d691234e12978a54237f
SLIDEboo <edgasmo@hotmail.com>
2025-11-23 15:34:05 +0100

    Update mods.js

--- mods.js
+++ mods.js
@@ -13,7 +13,7 @@
    "url": "https://drive.google.com/uc?export=download&id=12Z0deuYfx5Z1Kpnx7ShcVlfqbZ-q87gU&confirm=t",
    },
        {
-    "name": "lol"
+    "name": "lal"
    "url": "https://drive.google.com/uc?export=download&id=1I2P6cboSxE3taclEW_UvFIfB3K7ZI6m5&confirm=t",
    },
 
