10acc9e3ec34f33a1509b6c428ac59e7465423b2
SLIDEboo <edgasmo@hotmail.com>
2026-02-07 12:56:42 +0100

    Update mods.js

--- mods.js
+++ mods.js
@@ -2,17 +2,16 @@
  [
     {
      "name": "loool"
-   "url": "https://drive.google.com/uc?export=download&id=1OousP2Cls9R30zS3CqiYbqYW3zAlpjrZ&confirm=t",
+   "url": "https://drive.google.com/uc?export=download&id=1r10e83IfOlE7RE5qBKVvrVTC2JZl6RC9&confirm=t",
  },
     {
      "name": "lool"
- "url": "https://drive.google.com/uc?export=download&id=1cYQzWnk1vY3QQvSBlUx3-uGembowq2Nb&confirm=t",
+ "url": "https://drive.google.com/uc?export=download&id=1q7tHFbjd18VEjkv1KZYR2BF_XZD0yrIP&confirm=t",
  },
      {
     "name": "lol"
-   "url": "https://drive.google.com/uc?export=download&id=12Z0deuYfx5Z1Kpnx7ShcVlfqbZ-q87gU&confirm=t",
+   "url": "https://drive.google.com/uc?export=download&id=1g3eIXQUIOC1RH97i4l2AvB3VEW3YCCnr&confirm=t",
    },
 
   ]
 } 
-
