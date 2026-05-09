b3cf67cd3bcec59c3d5720a66eecaff21cb7d55f
SLIDEboo <edgasmo@hotmail.com>
2026-05-09 03:55:15 +0200

    Update mods.js

--- mods.js
+++ mods.js
@@ -1,17 +1,12 @@
-{
- [
-    {
-    "name": "aquamirae-6.API15",
-    "url": "https://github.com/SLIDEboo/sylventia/releases/download/mods/aquamirae-6.API15.jar"
- },
-    {
-    "name": "summoningrituals-neoforge-1.21.1-3.13.2",
-    "url": "https://github.com/SLIDEboo/sylventia/releases/download/mods/summoningrituals-neoforge-1.21.1-3.13.2.jar"
- },
-     {
-    "name": "lal"
-   "url": "https://drive.google.com/uc?export=download&id=1g3eIXQUIOC1RH97i4l2AvB3VEW3YCCnr&confirm=t",
-   },
+const mods = [
+  {
+    name: "aquamirae-6.API15",
+    url: "https://github.com/SLIDEboo/sylventia/releases/download/mods/aquamirae-6.API15.jar"
+  },
+  {
+    name: "summoningrituals-neoforge-1.21.1-3.13.2",
+    url: "https://github.com/SLIDEboo/sylventia/releases/download/mods/summoningrituals-neoforge-1.21.1-3.13.2.jar"
+  }
+];
 
-  ]
-} 
+module.exports = mods;
