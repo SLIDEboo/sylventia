5faee3adbdcfadab7e35e44f39e28a5c09f8e10f
SLIDEboo <edgasmo@hotmail.com>
2026-07-01 17:04:00 +0200

    Update mods.js

--- mods.js
+++ mods.js
@@ -4,6 +4,10 @@ const mods = [
   name: "SoundMuffler-Fabric-0.0.1.1.jar",
   url: "https://github.com/SLIDEboo/sylventia/releases/download/mods/SoundMuffler-Fabric-0.0.1.1.jar"
 },
+  {
+  name: "fabric-api-0.116.10+1.21.1",
+  url: "https://github.com/SLIDEboo/sylventia/releases/download/mods/fabric-api-0.116.10+1.21.1.jar"
+},
 ]
 ];
 
