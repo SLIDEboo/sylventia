c444965e04375704a4bf03ad33327e980d712d96
SLIDEboo <edgasmo@hotmail.com>
2026-07-29 18:39:38 +0200

    Update mods.js

--- mods.js
+++ mods.js
@@ -885,10 +885,6 @@ Sylventia:
     "name": "cobbledex-rei-emi-jei-fabric.jar",
     "url": "https://github.com/SLIDEboo/sylventia/releases/download/mods-pruebas/cobbledex-rei-emi-jei-fabric-2.0.3.1.jar"
 },
-  {
-    "name": "inventory-weight-2.0.2-1.21.jar",
-    "url": "https://github.com/SLIDEboo/sylventia/releases/download/mods-pruebas/inventory-weight-2.0.2-1.21.jar"
-},
 ]
 ];
 Pruebas:
